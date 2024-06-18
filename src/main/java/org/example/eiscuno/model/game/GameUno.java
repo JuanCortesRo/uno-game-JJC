7d0734bcc76edd2d12984758abf2fb5424b35e4a
Miguel Casanova <miguel.felipe.casanova@cali.edu.co>
2024-06-18 18:49:38 -0500

    The initial number of cards for player and machine is now 7

--- src/main/java/org/example/eiscuno/model/game/GameUno.java
+++ src/main/java/org/example/eiscuno/model/game/GameUno.java
@@ -37,8 +37,8 @@ public class GameUno implements IGameUno {
      */
     @Override
     public void startGame() {
-        for (int i = 0; i < 10; i++) {
-            if (i < 5) {
+        for (int i = 0; i < 14; i++) {
+            if (i < 7) {
                 humanPlayer.addCard(this.deck.takeCard());
             } else {
                 machinePlayer.addCard(this.deck.takeCard());
